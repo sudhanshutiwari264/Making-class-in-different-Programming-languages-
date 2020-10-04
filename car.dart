76e78913ec9e8ad7d6bcc8c0f37c2e6ec1cd9f26
Nikhilananddev <58386262+Nikhilananddev@users.noreply.github.com>
2020-10-04 21:14:51 +0530

    Update car.dart
    updates all fields

--- car.dart
+++ car.dart
@@ -1,10 +1,12 @@
 void main() { 
-   Car c= new Car(); 
-   c.disp(); 
+   Car car1 = new Car(); 
+ car1.disp(); 
 }  
 class Car {  
    // field 
-   String engine = "E1001";  
+   String engine = "008x5";  
+   int price =500;
+   String colour ="Red";
    
    // function 
    void disp() { 
