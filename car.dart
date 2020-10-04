fe3d5b630512b427c629048740580b00bf9e5817
Nikhilananddev <58386262+Nikhilananddev@users.noreply.github.com>
2020-10-04 18:51:20 +0530

    Create car.dart
    class in Dart

---
+++ car.dart
@@ -0,0 +1,13 @@
+void main() { 
+   Car c= new Car(); 
+   c.disp(); 
+}  
+class Car {  
+   // field 
+   String engine = "E1001";  
+   
+   // function 
+   void disp() { 
+      print(engine); 
+   } 
+}
