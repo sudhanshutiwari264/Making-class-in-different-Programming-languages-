6f4f2768af7ee38bb21e54e28aa3235597a29107
Dan Goodman <danthegoodman@icloud.com>
2020-10-05 15:27:55 -0400

    Added JSDocs JSDocs are important to classes so users can understand the class and class methods without having to dive into the code

--- vehicle.js
+++ vehicle.js
@@ -1,6 +1,12 @@
 // Defining the class
 class Vehicle {
     // Constructor defines values that are used and stored within the class
+    /**
+     * @description A Vehicle Object
+     * @param {Number} price Price of the vehicle
+     * @param {String} color Color of the vehicle
+     * @param {String} model Model of the vehicle
+     */
     constructor(price, color, model) {
         // this. - refers to the class itself
         // Setting class values of price, color and model from paramters
@@ -9,6 +15,10 @@ class Vehicle {
         this.model = model;
     };
     // Creates new methord that can be used after initializing the class
+    /**
+     * @function
+     * @description Logs out vehicle information to console
+     */
     vehicleDetails() {
         // Function logs values
         console.log(`Color: ${this.color}`);
@@ -16,6 +26,8 @@ class Vehicle {
         console.log(`Model: ${this.model}`);
     };
 };
+
+module.exports.Vehicle = Vehicle
 
 // initializing the class, passing values to be used later
 let car = new Vehicle(500, 'Red', '008x5');
