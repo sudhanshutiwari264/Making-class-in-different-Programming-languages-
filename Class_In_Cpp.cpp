1d3000a5d39312f1b2cef677fa0a3a23f13f0df2
vishwajeet7200 <43901984+vishwajeet7200@users.noreply.github.com>
2020-10-06 20:50:29 +0530

    Update Class_In_Cpp.cpp
    simple example for cpp class

--- Class_In_Cpp.cpp
+++ Class_In_Cpp.cpp
@@ -1,58 +1,39 @@
+// Program to illustrate the working of
+// objects and class in C++ Programming
+
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <functional>
-#include <algorithm>
-#include <string>
-#include <cstdlib>
-#include <sstream>
 using namespace std;
-//Vehicle Class
-class Vehicle {
-public:
-   string model; // model
-   string color;  // color
-   int	price;  // price
-public:
-//Sets to default values
-Vehicle() {
-   model = " ";
-   color = " ";
-   price= 0;
-}
-Vehicle(string model, string color, int price) {
-   model = model;
-   color = color;
-   price = price;
-}
-void setModel(string model) {
-   model = model;
-}
-void setColor(string color) {
-   color = color;
-}
-void setPrice(int price) {
-   price = price;
-}
-string getModel() {
-   return model;
-}
-string getColor() {
-   return color;
-}
-int getPrice() {
-   return price;
-}
+
+// create a class
+class Room {
+
+   public:
+    double length;
+    double breadth;
+    double height;
+
+    double calculateArea() {
+        return length * breadth;
+    }
+
+    double calculateVolume() {
+        return length * breadth * height;
+    }
 };
-//creating objects
-int main()
-{
-	Vehicle Car1;
-	Car1.model = "008x5";
-	Car1.color ="Red";
-    Car1.price = 500;
-    cout<<Car1.model<<endl;
-    cout<<Car1.color<<endl;
-    cout<<Car1.price;
+
+int main() {
+
+    // create object of Room class
+    Room room1;
+
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+
     return 0;
 }
